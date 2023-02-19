4e6b36dec8afb8bfa555f11671f386b10f0e091d
Anthony <gioant1@hotmail.com>
2023-02-19 16:01:06 -0500

    fixed redirect path link

--- loader/loader.js
+++ loader/loader.js
@@ -11,7 +11,7 @@ $(document).ready(function () {
 
     setTimeout(function () {
         // Redirect to index.html
-        window.location.href = "index.html";
+        window.location.href = "../index.html";
     }, delayTime);
 });
 
