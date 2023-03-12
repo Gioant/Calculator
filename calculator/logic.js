70111916ea16b8ffc82186b9d600aaced5a3fba2
Anthony <gioant1@hotmail.com>
2023-03-12 15:05:40 -0400

    removed operator function for now

--- calculator/logic.js
+++ calculator/logic.js
@@ -14,24 +14,3 @@ function multiply(num1, num2) {
     return num1 * num2;
 }
 
-
-function operator(op, num1, num2) {
-    switch (op) {
-        case "+":
-            add(num1, num2);
-            break;
-
-        case "-":
-            subtract(num1, num2);
-            break;
-
-        case "/":
-            divide(num1, num2);
-            break;
-
-        case "*":
-            multiply(num1, num2);
-            break;
-
-    }
-}
