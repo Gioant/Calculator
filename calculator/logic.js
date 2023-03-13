01bb9d8c5b7ae29f46b0eb313f251e6c29b1be66
Anthony <gioant1@hotmail.com>
2023-03-13 01:32:55 -0400

    added necessary functions for logic

--- calculator/logic.js
+++ calculator/logic.js
@@ -50,15 +50,12 @@ function multiply(num1, num2) {
 
 
 //operation function
-const operation = function () {
+function operation() {
     num2 = Number(lastInput.join("").toString);
     console.log(num1);
     console.log(num2);
 
-    /*
-    1) if conditions to check operation
-    2) calls specific function and saves it in result variable
-    */
+    //check operator
     switch (operator) {
         case "+":
             result = add(num1, num2);
@@ -76,11 +73,104 @@ const operation = function () {
             return;
     }
 
-
-
+    //concat the result of operation
     topSc.textContent = checkIfFloat(num1) + " " + operator + " " + checkIfFloat(num2) + " =";
     bottomSc.textContent = checkIfFloat(result);
 
     num1 = result
     checker = true;
 };
+
+
+function display(btn) {
+    //clear num2 if prev equation resulted in 0;
+    if (bottomSc.textContent === "0") {
+        num2 = 0;
+    }
+
+    if (result) {
+        lastInput = [];
+        result = "";
+    }
+
+    if (lastInput.includes(".") && this.textContent === ".") {
+        return;
+    }
+
+    if (lastInput.length === 10) {
+        lastInput.length = 10;
+        return;
+    } else
+        lastInput.push(this.textContent);
+    bottomSc.textContent = lastInput.join("").toString();
+}
+
+
+function chainOperations(btn) {
+    num2 = Number(bottomSc.textContent);
+
+    switch (operator) {
+        case "+":
+            result = add(num1, num2);
+            break;
+        case "-":
+            result = subtract(num1, num2);
+            break;
+        case "x":
+            result = multiply(num1, num2);
+            break;
+        case "/":
+            result = divide(num1, num2);
+            break;
+        default:
+            return;
+    }
+
+    num1 = checkIfFloat(result);
+
+    //reset variables
+    num2 = 0;
+    lastInput = [];
+}
+
+function selectOperator(btn) {
+    if (bottomSc.textContent === "0") {
+        return;
+    }
+
+    if (num1 === 0 || checker === true) {
+        operator = btn.target.textContent;
+        num1 = Number(bottomSc.textContent);
+        num1.toFixed(2);
+        lastInput = [];
+        topScreen.textContent = checkIfFloat(num1) + ' ' + operator;
+        checker = false;
+    }
+
+    else if (num1) {
+        chainOperations();
+        operator = btn.target.textContent;
+        topScreen.textContent = checkIfFloat(result) + ' ' + operator;
+    }
+}
+
+//check number
+function checkIfFloat(num) {
+    let result;
+    if (Number.isInteger(num)) {
+        result = Number(num);
+    } else {
+        result = Number(num.toFixed(2));
+    }
+    return result;
+};
+
+
+
+//change numberSign
+function changeNumberSign () {
+    let num = Number(bottomSc,textContent);
+    num = num - (num * 2);
+    lastInput = [num];
+    bottomSc.textContent = num;
+}
