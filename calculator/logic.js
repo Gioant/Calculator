e75e3f15d87ebbaa9459bc9548690e46e7e9c9f1
Anthony <gioant1@hotmail.com>
2023-03-21 01:31:41 -0400

    Completed Logic for calculator! Currently Fixing Equal Function Bug
    Separated conditionals into different functions

--- calculator/logic.js
+++ calculator/logic.js
@@ -2,6 +2,7 @@
 let currentValue = 0;
 let previousValue = 0;
 let operator = null;
+let finalResult = null;
 
 //getting top & bottom screens 
 const topSc = document.querySelector("#previousInput");
@@ -12,33 +13,98 @@ const bottomSc = document.querySelector("#result");
 const numberBtns = document.querySelectorAll(".btn_num");
 const operatorBtns = document.querySelectorAll(".btn_operator");
 
+const clearEntryBtn = document.getElementById("clear-entry");
+const clearAllBtn = document.getElementById("clear-all");
+const decimalBtn = document.getElementById("decimal");
+const equalsBtn = document.getElementById("equals");
 
 
-//adding event listeners & calling appropriate function
-numberBtns.forEach(button => {
-    button.addEventListener('click', () => handleNumber(button.innerHTML));
-});
+function handleNumber(button) {
+    finalResult = null;
 
-operatorBtns.forEach(button => {
-    button.addEventListener('click', () => handleOperator(button.innerHTML));
-});
+    //update variable
+    currentValue = Number(currentValue + button.innerText);
 
-
-
-//function to clear everything
-function clear() {
-    bottomSc.textContent = "0";
-    topSc.textContent = "";
-    num1 = "";
-    num2 = "";
-    operation = "";
+    // Display to screen
+    bottomSc.innerText = currentValue;
 }
 
-//function to clear current input only
+function handleOperator(button) {
+
+    if ((operator !== null) && (currentValue === 0)) {
+        currentValue = 0;
+
+
+    } else if (operator === null) {
+        previousValue = currentValue + finalResult;
+
+        currentValue = 0;
+
+        //Display Results to Screen
+        topSc.innerText += bottomSc.innerText + button.innerText;
+        bottomSc.innerText = currentValue;
+
+
+    } else {
+        previousValue = operate(previousValue, currentValue, operator);
+
+        currentValue = 0;
+
+        //Display Results to Screen
+        topSc.innerText += bottomSc.innerText + button.innerText;;
+        bottomSc.innerText = currentValue;
+    }
+}
+
+function handleDecimal() {
+    if (!currentValue.toString().includes('.')) {
+        currentValue += '.';
+    }
+
+    // Display result
+    return bottomSc.innerText = currentValue;
+}
+
+function handleEquals() {
+    if (operator === null) {
+        return currentValue;
+    } else {
+
+        //call operator function
+        finalResult = operate(previousValue, currentValue, operator);
+
+        //reset variables
+        currentValue = 0;
+        previousValue = 0;
+        operator = null;
+        topSc.innerText = null;
+
+        // display current result
+        return bottomSc.innerText = finalResult;
+    }
+}
+
+//Function to clear current Entry
 function clearEntry() {
-    bottomSc.textContent = bottomSc.textContent.toString().slice(0, 1);
+    //delete last character
+    currentValue = Number(currentValue.toString().slice(0, -1));
+
+    return bottomSc.innerText = currentValue;
 }
 
+//Clear Everything Function
+function clearAll() {
+
+    //reset Everything
+    currentValue = 0;
+    previousValue = 0;
+    finalResult = null;
+    operator = null;
+
+    //reset screens
+    bottomSc.innerText = null;
+    topSc.innerText = null;
+}
 
 
 //Math functions
@@ -57,3 +123,31 @@ function operate(operand, a, b) {
             return a / 100 * b;
     }
 }
+
+//adding event listeners for number & operator buttons
+numberBtns.forEach(button => {
+    button.addEventListener('click', () => handleNumber(button));
+});
+
+operatorBtns.forEach(button => {
+    button.addEventListener('click', () => handleOperator(button));
+});
+
+
+//add event listeners to the individual buttons
+equalsBtn.addEventListener("click", () => {
+    handleEquals();
+});
+
+decimalBtn.addEventListener("click", () => {
+    handleDecimal();
+});
+
+clearAllBtn.addEventListener("click", () => {
+    clearAll();
+});
+
+clearEntryBtn.addEventListener("click", () => {
+    clearEntry();
+});
+
